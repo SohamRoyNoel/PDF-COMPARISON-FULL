5ba31de5bdcdae942a1c66f80c21a66a8940fe70
Soham Roy <36095087+SohamRoyNoel@users.noreply.github.com>
2020-05-09 04:03:10 +0530

    Update ResourcePaths.java

--- src/com/PDF_Validator/main/resourcePath/ResourcePaths.java
+++ src/com/PDF_Validator/main/resourcePath/ResourcePaths.java
@@ -2,8 +2,8 @@ package com.PDF_Validator.main.resourcePath;
 
 public class ResourcePaths {
 
-	public static String sourcePath = "\\\\ENTSSERVER85\\Cognizant\\Soham\\PDF_Comparison_Execution\\Source";
-	public static String targetPath = "\\\\ENTSSERVER85\\Cognizant\\Soham\\PDF_Comparison_Execution\\Target";
-	public static String resultPath = "\\\\ENTSSERVER85\\Cognizant\\Soham\\PDF_Comparison_Execution\\Results";
-	public static String outputValidationResultPath = "\\\\ENTSSERVER85\\Cognizant\\Soham\\PDF_Comparison_Execution\\Results\\Alignment_Validation_Error_Report";
+	public static String sourcePath = "\\Source";
+	public static String targetPath = "\\Target";
+	public static String resultPath = "\\Results";
+	public static String outputValidationResultPath = "\\Alignment_Validation_Error_Report";
 }
